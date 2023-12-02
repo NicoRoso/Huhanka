889560ffd56a50f9b86f0e8711eb5703bcc6ef04
El_Nico <118479527+NicoRoso@users.noreply.github.com>
2023-12-02 15:09:22 +0300

    Upgrade

---
+++ src/Assets/Nabiulin/DeathTime/Scripts/TimeDeath.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDeath : MonoBehaviour
+{
+    [SerializeField]
+    private float _timer;
+
+    [SerializeField]
+    private Slider _slider;
+
+    [SerializeField]
+    private GameObject _canvasTime;
+
+    [SerializeField]
+    private GameObject _canvasDeath;
+
+    [SerializeField]
+    private bool isStarted;
+
+    private void Start()
+    {
+        isStarted = false;
+        _canvasTime.SetActive(false);
+        _canvasDeath.SetActive(false);
+
+       //_timer = 60f;
+
+        _slider.maxValue = _timer;
+        _slider.value = _timer;
+    }
+
+    private void Update()
+    {
+        if (isStarted)
+        {
+            _canvasTime.SetActive(true);
+            _timer -= Time.deltaTime;
+
+            if (_timer <= 0)
+            {
+                _canvasTime.SetActive(false);
+                _canvasDeath.SetActive(true);
+                _timer = 0;
+
+            }
+
+            if (_slider.value < _slider.maxValue / 2.5f)
+            {
+                _slider.GetComponent<Animator>().SetTrigger("Critical");
+            }
+            _slider.value = _timer;
+        }
+    }
+
+    public void StartTimer(bool isStarted)
+    {
+        isStarted = true;
+    }
+}
