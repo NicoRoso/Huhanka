f10ae92b1cbecb81912c3beba1bdaf3d8334afcc
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-12-02 04:18:26 +0300

    otherRotationMovement_1

--- src/Assets/Player.cs
+++ src/Assets/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform skin;
     private CharacterController _characterController;
     private Vector3 _move;
     private Vector3 _gVelocity;
@@ -29,10 +30,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundLayerMask);
-
+        Debug.DrawLine(_groundCheck.position+_groundCheck.transform.up*-1*_groundDistance, _groundCheck.position+_groundCheck.transform.up*_groundDistance);
         if (_isGrounded && _gVelocity.y < 0)
         {
-            _gVelocity.y = -0.1f;
+            _gVelocity.y = -0.5f;
         }
 
         _move = transform.right* Input.GetAxis("Horizontal")+transform.forward* Input.GetAxis("Vertical");
@@ -41,9 +42,12 @@ public class Player : MonoBehaviour
         _mouseInput = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
         _xRotation -= _mouseInput.y*_mouseSensitivity*Time.deltaTime;
         _camera.localRotation = Quaternion.Euler(Math.Clamp(_xRotation, -90f, 90f), 0f, 0f);
-        transform.Rotate(Vector3.up*_mouseInput.x*_mouseSensitivity*Time.deltaTime);
         
+        transform.Rotate(-skin.forward*_mouseInput.x*_mouseSensitivity*Time.deltaTime);
+        Debug.DrawRay(skin.position, skin.up, Color.cyan);
         _gVelocity.y += _gravity * Time.deltaTime;
+        _gVelocity = Vector3.ProjectOnPlane(_gVelocity, skin.forward);
         _characterController.Move(_gVelocity * Time.deltaTime);
+
     }
 }
