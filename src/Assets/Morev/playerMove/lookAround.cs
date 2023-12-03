707727246180ee572b34833bf6303b5f68400b97
OlegMorev <proks200413@gmail.com>
2023-12-03 21:32:46 +0300

    jump

--- src/Assets/Morev/playerMove/lookAround.cs
+++ src/Assets/Morev/playerMove/lookAround.cs
@@ -20,7 +20,7 @@ public class lookAround : MonoBehaviour
         mouseY = Input.GetAxis("Mouse Y") * intensivity * Time.deltaTime;
         xRotation += mouseX;
         yRotation -= mouseY;
-        yRotation = Mathf.Clamp(yRotation, -89.9f, 89.9f);
+        yRotation = Mathf.Clamp(yRotation, -89f, 89f);
         transform.rotation = Quaternion.Euler(yRotation, xRotation, 0);
     }
 
