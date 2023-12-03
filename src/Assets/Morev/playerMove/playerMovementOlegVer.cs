36bf35acd1ba739e8fe91d16a7463d705f466034
OlegMorev <proks200413@gmail.com>
2023-12-03 18:03:05 +0300

    new player gravity system

--- src/Assets/Morev/playerMove/playerMovementOlegVer.cs
+++ src/Assets/Morev/playerMove/playerMovementOlegVer.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class playerMovementOlegVer : MonoBehaviour
@@ -11,16 +12,33 @@ public class playerMovementOlegVer : MonoBehaviour
     float z;
     Vector3 move;
     Vector3 gravity;
-    private void Start()
-    {
-        gravity = new Vector3(0, -1, 0);
-    }
+    float fallSpeed = 0;
+    public float gravityCoefficient = 1;
     void Update()
     {
+        gravity = new Vector3(0, -fallSpeed, 0);
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
         _stabilizator.eulerAngles = new Vector3(0, _stabilizator.eulerAngles.y, 0);
         move = _stabilizator.right * x + _stabilizator.forward * z + gravity;
         characterController.Move(move * _speed * Time.deltaTime);
+    }
+    float time = 0;
+    private void FixedUpdate()
+    {
+        if (!characterController.isGrounded)
+        {
+            time += Time.deltaTime * gravityCoefficient * 9.8f;
+            fallSpeed = time;
+        }
+        else
+        {
+            time = 0;
+            fallSpeed = 0;
+        }
+    }
+    public void SetGravityCoefficient(float newCoefficient)
+    {
+        gravityCoefficient = newCoefficient;
     }
 }
