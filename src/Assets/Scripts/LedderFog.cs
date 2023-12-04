77f5f34065e79a21752d6435457bc8a8355b1296
KotovSergeyV <75029707+KotovSergeyV@users.noreply.github.com>
2023-12-04 17:09:33 +0300

    FogCommitFix

--- src/Assets/Scripts/LedderFog.cs
+++ src/Assets/Scripts/LedderFog.cs
@@ -9,9 +9,9 @@ public class LedderFog : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<TimeDeath>().StartTimer(false, 0f);
-            RenderSettings.fogColor = new Color(0.5f, 0.5f, 0.5f, 1f);
-            RenderSettings.fogDensity = 0.04f;
-            RenderSettings.fogMode = FogMode.ExponentialSquared;
+            // RenderSettings.fogColor = new Color(0.5f, 0.5f, 0.5f, 1f);
+            // RenderSettings.fogDensity = 0.04f;
+            // RenderSettings.fogMode = FogMode.ExponentialSquared;
         }
     }
 }
