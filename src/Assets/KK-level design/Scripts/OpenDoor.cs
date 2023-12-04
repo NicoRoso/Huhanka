77809a8152732656642a3c61c05f813960c4fe0a
OlegMorev <proks200413@gmail.com>
2023-12-04 22:01:43 +0300

    steps

--- src/Assets/KK-level design/Scripts/OpenDoor.cs
+++ src/Assets/KK-level design/Scripts/OpenDoor.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 
 public class OpenDoor : MonoBehaviour
 {
+    bool isOpenable = false;
+    private void Start()
+    {
+        StartCoroutine(StartTimer(38));
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && isOpenable)
         {
             GetComponent<Animator>().SetTrigger("Open");
+            GetComponent<AudioManager>().Play("open");
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && isOpenable)
         {
             GetComponent<Animator>().SetTrigger("Close");
+            GetComponent<AudioManager>().Play("close");
         }
+    }
+    IEnumerator StartTimer(int time)
+    {
+        yield return new WaitForSeconds(time);
+        isOpenable = true;
     }
 }
