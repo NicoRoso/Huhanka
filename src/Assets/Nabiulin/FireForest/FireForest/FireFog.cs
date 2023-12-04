d392d7cef490e771cb3fa4bb0f047337d6149139
El_Nico <118479527+NicoRoso@users.noreply.github.com>
2023-12-04 04:02:48 +0300

    FixedHud+DeathScreen

--- src/Assets/Nabiulin/FireForest/FireForest/FireFog.cs
+++ src/Assets/Nabiulin/FireForest/FireForest/FireFog.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class FireFog : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _O2;
+    [SerializeField]
+    private GameObject _snow;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            _O2.SetActive(true);
+            _snow.SetActive(false);
             other.gameObject.GetComponent<TimeDeath>().StartTimer(true, 80f);
             RenderSettings.fog = true; //C59A4B ColorRender
             RenderSettings.fogColor = new Color(0.772549f, 0.6039216f, 0.2941177f);
--- src/Assets/Nabiulin/WinterWorldMap/TriggerForTimer.cs
+++ src/Assets/Nabiulin/WinterWorldMap/TriggerForTimer.cs
@@ -7,11 +7,19 @@ public class TriggerForTimer : MonoBehaviour
     [SerializeField]
     private GameObject _blizzard;
 
+    [Header("HUD")]
+    [SerializeField]
+    private GameObject _O2;
+    [SerializeField]
+    private GameObject _snow;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             _blizzard.SetActive(true);
+            _O2.SetActive(false);
+            _snow.SetActive(true);
             other.gameObject.GetComponent<TimeDeath>().StartTimer(true, 120f);
             RenderSettings.fog = true; //808080 ColorRender
             RenderSettings.fogColor = new Color(0.5019608f, 0.5019608f, 0.5019608f);
